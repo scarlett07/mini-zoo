de70ea770f02269caadcf67003b749775703927c
scarlett07 <scarlett.geo@gmail.com>
2017-11-29 23:12:11 -0600

    terminando el cambio de todos los filtros

--- js/app.js
+++ js/app.js
@@ -1,14 +1,31 @@
 var cambiarFiltro = function() {
-  var imagen = document.getElementsByClassName('animal');// accedo a todas las imagenes
-  var filtro = document.getElementById("selector").selectedIndex; //accedo al index donde se enciuentra el texto del filtro
+  var imagen = document.getElementsByTagName('img');// accedo a todas las imagenes
+  var filtro = document.getElementById('selector').value; //accedo al index donde se enciuentra el texto del filtro
 
-  if (document.getElementsByTagName("option")[filtro].value =='negative-colors'){ //comparo el filtro para saber cual será aplicado
-    for(i=0;i<imagen.length;i+2){ //recorro cada imagen para que se le aplique a todas
-        imagen[i].setAttribute("class", "negative-colors");
- }  //   } else if (document.getElementsByTagName("option")[filtro].value =='Sepia') {
-  //     for(i=0;i<imagen.length;i+2){
-  //         imagen[i].setAttribute("class", "sepia");
-  //   }
-  // }
-}
+  var newImagen=[];
+
+  if (filtro =='negative-colors') { //comparo el filtro para saber cual será aplicado
+    for(var i=0;i<imagen.length;i++){ //recorro cada imagen para que se le aplique a todas
+      newImagen.push(imagen[i].setAttribute('class', 'negative-colors'));
+      }
+      return newImagen;
+    }
+  else if (filtro =='Sepia') {
+    for(var i=0;i<imagen.length;i++){
+      newImagen.push(imagen[i].setAttribute('class', 'sepia'));
+      }
+      return newImagen;
+    }
+  else if (filtro =='White-black') {
+    for (var i = 0; i < imagen.length; i++) {
+      newImagen.push(imagen[i].setAttribute('class', 'white-black'));
+    }
+    return newImagen;
+  }
+  else if(filtro =='Original') {
+    for (var i = 0; i < imagen.length; i++) {
+      newImagen.push(imagen[i].setAttribute('class', 'original'));
+    }
+    return newImagen;
+  }
 }
